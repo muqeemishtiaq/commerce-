56bb85356328ea21d0c45d3ecd8f896755fb1da3
Muqeem Ishtiaq <muqeemishtiaq1234@gmail.com>
2025-10-31 12:42:06 +0500

    recheck

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -29,7 +29,7 @@ const Home = ({ setProductId }) => {
                   className="mt-2 bg-green-500 text-white text-xs px-3 py-1 rounded hover:bg-blue-600 cursor-pointer"
                   onClick={() => {
                     setProductId(product.id);
-                    toast.success('Product');
+                    toast.success('Product is added');
                   }}
                 >
                   Add to Cart
